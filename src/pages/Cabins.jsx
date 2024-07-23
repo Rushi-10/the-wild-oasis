a900bf1af018d8a9fef155c714872b7b155050c9
rushi10 <rushimadhure10@gmail.com>
2024-07-23 23:42:12 +0530

    Updated using advance pattern like compound component,render props

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -2,11 +2,10 @@ import { useState } from "react";
 import CabinTable from "../features/cabins/CabinTable";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
-import Button from "../ui/Button";
-import CreateCabinForm from "../features/cabins/CreateCabinForm";
+import AddCabins from "../features/cabins/AddCabins";
 
 function Cabins() {
-  const [showForm, setShowForm] = useState(false);
+  
 
   return (
     <>
@@ -18,10 +17,7 @@ function Cabins() {
       <Row>
         <CabinTable />
 
-        <Button onClick={() => setShowForm((show) => !show)}>
-          Add new cabin
-        </Button>
-        {showForm && <CreateCabinForm />}
+       <AddCabins/>
       </Row>
     </>
   );
